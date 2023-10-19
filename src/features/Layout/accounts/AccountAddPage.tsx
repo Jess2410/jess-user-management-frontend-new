65932ebb6b3eaead06286122d3e176dd3be8a811
JessicaTOUNKARA <jessica.tounkara@kpconsulting.fr>
2023-10-19 17:42:21 +0200

    feat: Hook Add Account

--- src/features/Layout/accounts/AccountAddPage.tsx
+++ src/features/Layout/accounts/AccountAddPage.tsx
@@ -1,10 +1,16 @@
 import AccountForm from "../../../components/accountForm/AccountForm";
 import Layout from "../Layout";
+import UseAccountAdd from "./hooks/useAccountAdd";
 
 const AccountAddPage = () => {
+  const { createAccount, selectedRoles, setSelectedRoles } = UseAccountAdd();
   return (
     <Layout>
-      <AccountForm />
+      <AccountForm
+        onSubmit={createAccount}
+        selectedRoles={selectedRoles}
+        setSelectedRoles={setSelectedRoles}
+      />
     </Layout>
   );
 };
